26a51a1de88e84eb78e3812587aef549f0653996
Sjoerd Langkemper <sjoerd-github@linuxonly.nl>
2019-12-06 20:22:36 +0100

    Replace words of two characters by fixed string

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+extern crate rayon;
+
+use std::env;
+use std::collections::HashSet;
+use rayon::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let args : Vec<String> = env::args().collect();
+    assert!(args.len() > 1);
+    let results : Vec<String> = args[1..].iter().map(compress).collect();
+    println!("{:?}", results);
+}
+
+fn compress(text: &String) -> std::string::String {
+    let mut words : HashSet<String> = HashSet::new();
+    for i in 0..text.len() - 1 {
+        let word =  &text[i..i+2];
+        if !word.contains(' ') {
+            if text.matches(word).count() > 1 {
+                words.insert(word.to_string());
+            }
+        }
+    }
+    let mut result = String::from(text);
+    for word in words {
+        result = result.replace(&word, "%");
+    }
+    // println!("{}", result);
+    return result;
 }
